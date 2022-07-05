7c58dd9c20bf824beafd50224f42972cd7fe8880
amlsmm <amlsmm0@gmail.com>
2022-07-05 08:23:55 -0300

    Criacao classes A e B

---
+++ B.java
@@ -0,0 +1,29 @@
+public class B{
+    private int B1;
+    private double B2;
+
+    public int getB1(){
+        return B1;
+    }
+
+    public double getB2(){
+        return B2;
+    }
+
+    public void setB1(int b1){
+        this.B1 = b1;
+    }
+
+    public void setB2(double b2){
+        this.B2 = b2;
+    }
+
+    public void MB1(){
+        System.out.println("Metodo MB1");
+    }
+
+    public void MB2(){
+        System.out.println("Metodo MB2");
+    }
+
+}
